b2c65e2f7bd4578771181980bc170b70ba76f56a
Chimit Menzhikov <chimit@mosk.ru>
2020-02-01 14:17:53 +0700

    Added step 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import question from '..';
+
+const greetings = () => {
+    console.log('Welcome to the Brain Games!');
+}
+
+greetings();
+question();
